11f01c4cb24fe54117f074660b25156379cf92cc
System <none>
2017-05-17 11:42:58 +0000

    Checkpoint

--- server.js
+++
@@ -1,8 +0,0 @@
-var routeParser = require("route-parser");
-
-var req = {path: '/api/articles?tag=vuejs'};
-
-var thisRoute = '/api/articles?tag=:tag';
-var route = new routeParser(thisRoute);
-var matchedPathKeys = route.match(req.path);
-console.log(matchedPathKeys);
