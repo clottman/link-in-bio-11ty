3a1ef86159effc276d9eab118f33861e8c847246
Glitch (eleventy-on-glitch) <none>
2018-06-07 02:30:11 +0000

    🍓⛰ Checkpoint ./package.json:1258895/186 ./cool-file.js:1258895/28 ./.eleventy.js:1258895/421 ./README.md:1258895/592

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+// docs: https://www.11ty.io/docs/config/
+
+module.exports = function(eleventyConfig) {
+  
+  // eleventyConfig.addFilter( "myFilter", function() {});
+ 
+  return {
+    dir: {
+      input: "src",
+      output: "dist"
+    }
+  };
+};
