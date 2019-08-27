961330d20133819ff98d50712a1df5e257bbf70e
Glitch (first-end) <none>
2019-08-27 17:34:30 +0000

    🕴👠 Checkpoint ./src/_includes/layouts/default.njk:708175/115 ./src/site.css:708175/1078 ./.eleventy.js:708175/119 ./src/css/site.css:708175/1174 ./src/index.html:708175/140

--- .eleventy.js
+++ .eleventy.js
@@ -23,8 +23,10 @@ module.exports = function(eleventyConfig) {
     },
     }
   });
+  eleventyConfig.addPassthroughCopy("src/css");
   
   return {
+    passthroughFileCopy: true,
     dir: {
       input: "src",
       output: "dist"
